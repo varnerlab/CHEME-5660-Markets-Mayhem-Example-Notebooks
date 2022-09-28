458cb6c3c01bd88e0938e89dfb0ac744e3215cd7
Jeffrey Varner <jdv27@cornell.edu>
2022-09-28 10:01:08 -0400

    Lab 2
    Lab 2 notebook

---
+++ labs/lab-2-Binomial-Pricing-Single-Assets/.ipynb_checkpoints/CHEME-5660-Lab-2-Library-checkpoint.jl
@@ -0,0 +1,5 @@
+function compute_indexes_for_level(connectivity::Array{Int64,2},level::Int64)::Dict{Int64,Array{Int64,1}}
+end
+
+function compute_return_and_volatility(data::DataFrame; m::Int64=30)::Tuple{Float64,Float64}
+end
